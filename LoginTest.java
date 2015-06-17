e6bdb2bfd8db92ee971a6ff025ea3ad724b47eb9
Chandan Nayak <chandan.nayak@synerzip.com>
2015-06-17 14:41:35 +0530

    Create LoginTest.java

---
+++ LoginTest.java
@@ -0,0 +1,49 @@
+package stack1;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	
+	@Test public static void golog1(){
+		
+		System.out.println("golog1test1");
+		
+	}
+	
+	@Test public static void golog2(){
+		
+		System.out.println("golog2test2");
+		
+	}
+	
+	@Test public static void golog3(){
+		
+		System.out.println("golog3test3");
+		
+	}
+	
+	@Test public static void golog4(){
+		
+		System.out.println("golog4test4");
+		
+	}
+	
+	@Test public static void golog5(){
+		
+		System.out.println("golog5test5");
+		
+	}
+	
+	@Test public static void golog6(){
+		
+		System.out.println("golog6test6");
+		
+	}
+	
+	@Test public static void golog7(){
+		
+		System.out.println("golog7test7");
+		
+	}
+
+}
